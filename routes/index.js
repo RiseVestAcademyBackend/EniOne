0a6c80d89fc9efc380587a5d2f04e3c54f264c5b
xutini <110780981+xutini@users.noreply.github.com>
2025-01-31 23:50:11 +0100

    home page link update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'EniOne' });
+  res.render('index', { title: 'EniOne', links: ["/namerufai", "/namefestus", ] });
 });
 
 router.get('/namerufai', function(req, res, next) {
