be5f23ac05a55b972dd8745bbd7d4933d6091b71
Ebuka <ebukaobiora10@gmail.com>
2025-02-06 13:23:46 +0100

    added routes for models and their brands

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var modelBrands = require('./routes/modelsBrands')
 
 var app = express();
 
@@ -21,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', modelBrands);
+app.use('/', modelBrands);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
